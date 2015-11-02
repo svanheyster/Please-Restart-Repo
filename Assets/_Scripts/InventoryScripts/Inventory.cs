c328f7d083321007af3a563d21c8f5b1723e40eb
emilbyskovnielsen <emil.byskov@me.com>
2015-11-02 23:43:54 +0100

    Inventory update

--- Assets/_Scripts/InventoryScripts/Inventory.cs
+++ Assets/_Scripts/InventoryScripts/Inventory.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class Inventory : MonoBehaviour {
 	GameObject inventoryPanel;
 	GameObject slotPanel;
+	ItemDatabase database;
 	public GameObject inventorySlot;
 	public GameObject inventoryItem;
 
@@ -14,13 +15,32 @@ public class Inventory : MonoBehaviour {
 
 	void Start () 
 	{
+		database = GetComponent<ItemDatabase> ();
 		slotAmount = 3;
 		inventoryPanel = GameObject.Find ("Inventory Panel");
 		slotPanel = inventoryPanel.transform.FindChild ("Slot Panel").gameObject;
 		for (int i = 0; i < slotAmount; i++) 
 		{
+			items.Add(new Item());
 			slots.Add (Instantiate(inventorySlot));
 			slots[i].transform.SetParent(slotPanel.transform);
+		}
+
+		AddItem (0);
+	}
+
+	public void AddItem(int id)
+	{
+		Item itemToAdd = database.FetchItemByID (id);
+		for (int i = 0; i < items.Count; i++) 
+		{
+			if(items[i].ID == -1)
+			{
+				items[i] = itemToAdd;
+				GameObject itemObj = Instantiate(inventoryItem);
+				itemObj.transform.SetParent(slots[i].transform);
+				itemObj.transform.position = Vector2.zero;
+			}
 		}
 	}
 }

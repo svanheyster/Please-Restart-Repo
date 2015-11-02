e6f99cb700d98c305c8ff269204ad58f97096906
emilbyskovnielsen <emil.byskov@me.com>
2015-11-02 23:06:30 +0100

    Minor Update

--- Assets/_Scripts/InventoryScripts/Inventory.cs
+++ Assets/_Scripts/InventoryScripts/Inventory.cs
@@ -1,14 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Inventory : MonoBehaviour {
 	GameObject inventoryPanel;
 	GameObject slotPanel;
-	GameObject inventorySlot;
-	GameObject inventoryItem;
+	public GameObject inventorySlot;
+	public GameObject inventoryItem;
 
+	private int slotAmount;
+	public List<Item> items = new List<Item>();
+	public List<GameObject> slots = new List<GameObject> ();
 
-	void Start () {
-	
+	void Start () 
+	{
+		slotAmount = 3;
+		inventoryPanel = GameObject.Find ("Inventory Panel");
+		slotPanel = inventoryPanel.transform.FindChild ("Slot Panel").gameObject;
+		for (int i = 0; i < slotAmount; i++) 
+		{
+			slots.Add (Instantiate(inventorySlot));
+			slots[i].transform.SetParent(slotPanel.transform);
+		}
 	}
 }

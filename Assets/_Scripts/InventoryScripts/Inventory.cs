cd857bf45848220113d1c67d53e116835b9fc3fa
emilbyskovnielsen <emil.byskov@me.com>
2015-11-02 21:38:03 +0100

    Revert "shit"
    This reverts commit 5f4462fc2c228f152d1440be945b67b21aad9eab.

--- Assets/_Scripts/InventoryScripts/Inventory.cs
+++ Assets/_Scripts/InventoryScripts/Inventory.cs
@@ -6,7 +6,6 @@ public class Inventory : MonoBehaviour {
 
 	GameObject inventoryPanel;
 	GameObject slotPanel;
-	ItemDatabase database;
 	GameObject inventorySlot;
 	GameObject inventoryItem;
 
@@ -16,13 +15,11 @@ public class Inventory : MonoBehaviour {
 
 	void Start () 
 	{
-		database = GetComponent<ItemDatabase> ();
-
 		slotAmount = 3;
 		inventoryPanel = GameObject.Find ("Inventory Panel");
 		slotPanel = inventoryPanel.transform.FindChild ("Slot Panel").gameObject;
 
-		//items.Add (new Item ());
+		items.Add (new Item ());
 		slots.Add (GameObject.Find ("Slot1"));
 		slots.Add (GameObject.Find ("Slot2"));
 		slots.Add (GameObject.Find ("Slot3"));
@@ -30,18 +27,7 @@ public class Inventory : MonoBehaviour {
 
 	public void AddItem(int id)
 	{
-		Item itemToAdd = database.FetchItemByID (id);
-		for (int i = 0; i < items.Count; i++) 
-		{
-			if(items[i].ID == -1)
-			{
-				items[i] = itemToAdd;
-				//GameObject itemObj = Instantiate(InventoryItem);
-				//itemObj.transform.SetParent(slots[i].transform);
-				//itemObj.transform.position = Vector2.zero;
-				break;
-			}
-		}
+
 	}
 
 	void Update () {
